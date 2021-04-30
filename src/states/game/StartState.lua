8d21d466ff45bc5b716d933f6757891612b3f531
RafaelWO <weingartner.rafael@hotmail.com>
2021-04-30 16:57:59 +0200

    initial commit

---
+++ src/states/game/StartState.lua
@@ -0,0 +1,24 @@
+StartState = Class{__includes = BaseState}
+
+function StartState:init()
+    self.name = "StartState"
+    self.backgroundSounds = {'laser-1', 'laser-2', 'two-tone'}
+    Timer.every(5, 
+        function () 
+            local soundIdx = math.random(#self.backgroundSounds)
+            print(soundIdx)
+            gSounds[self.backgroundSounds[soundIdx]]:play()
+        end
+    )
+end
+
+function StartState:update(dt)
+
+end
+
+function StartState:render()
+    love.graphics.setBackgroundColor(0.5,0,1)
+    love.graphics.setColor(34, 34, 34, 255)
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf('Space Love', 0, VIRTUAL_HEIGHT / 2 - 30, VIRTUAL_WIDTH, 'center')
+end
