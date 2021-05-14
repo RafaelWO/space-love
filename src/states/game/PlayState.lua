e16bd2946a2707a4c73b22ac19cae298b1fb46a6
RafaelWO <weingartner.rafael@hotmail.com>
2021-05-14 08:32:46 +0200

    Removed old code

--- src/states/game/PlayState.lua
+++ src/states/game/PlayState.lua
@@ -10,6 +10,5 @@ end
 
 function PlayState:render()
     love.graphics.printf('Playing...', 0, VIRTUAL_HEIGHT / 2 - 30, VIRTUAL_WIDTH, 'center')
-    local sheet = love.graphics.newImage('SpaceShooterRedux/Spritesheet/sheet.png')
     love.graphics.draw(gTextures['sheet'], gFrames['sheet']['playerShip1_blue'], 0, 0)
 end
